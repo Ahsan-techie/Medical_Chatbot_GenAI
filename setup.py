a8141eec0ea7a283f8c93e051302a7dc6a8d8804
ahsan <unknownuser34754@gmail.com>
2025-04-05 05:39:15 +0100

    Project structure created added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI Medical Bot",
+    version="1.0.0",
+    description="A chatbot for generating medical advice based on user inputs.",
+    author="Ahsan",
+    author_email="chahsan1393@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
